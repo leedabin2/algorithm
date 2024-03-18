6c8245769464a48a9effbc1adc326656a7cbfcbc
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2024-03-18 17:35:01 +0900

    [Silver II] Title: 외판원 순회 2, Time: 1224 ms, Memory: 114336 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -0,0 +1,42 @@
+import sys
+n = int(sys.stdin.readline())
+
+graph = [[] for _ in range(n)]
+
+map_city = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+
+visited = [False] * n
+min_cost = float("INF")
+
+# 모든 depth를 한 번씩만 방문 / 출발도시로 돌아오는 경로 고려
+def dfs(city, path, cost):
+    global min_cost
+
+    # 모든 도시를 방문했을 경우
+    if len(path) == n:
+        # 출발 도시로 돌아가는 비용 추가
+        return_cost = map_city[city][path[0]]
+        if return_cost > 0:
+            min_cost = min(min_cost, cost + return_cost)
+        return
+
+    for next_city in range(n):
+        # 방문하지 않았고, 이동할 수 있는 경우
+        if not visited[next_city] and map_city[city][next_city] > 0:
+            visited[next_city] = True
+            dfs(next_city, path + [next_city], cost + map_city[city][next_city])
+            visited[next_city] = False
+
+
+# 각 노드에 연결된 정보 저장(노드,비용)
+for i in range(n):
+    for j in range(len(map_city)):
+        graph[i].append((j,map_city[i][j]))
+
+# 각 도시마다 방문
+for i in range(n):
+    visited[i] = True
+    dfs(i,[i],0)
+    visited[i] = False
+
+print(min_cost)
