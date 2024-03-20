ceb7f1b0933ee00523276fe540b957195f3e1bf9
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2024-03-20 17:34:37 +0900

    [Bronze I] Title: 더하기 사이클, Time: 56 ms, Memory: 39820 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,4 +1,5 @@
-import sys
+import sys,random,itertools
+from itertools import permutations
 
 N = int(sys.stdin.readline().rstrip())
 
@@ -11,13 +12,13 @@ def dfs(n,count):
 
     if n == N and count >0:
         cnt = count
-        return cnt
+        return 
 
     else:
         a = n // 10  # 첫번째 값
         b = n - ((n // 10) * 10)  # 두번째 값
         new = b * 10 + (a+b) % 10
-        return dfs(new,count+1)
+        dfs(new,count + 1)
 
-res = dfs(N,0)
-print(res)
+dfs(N,0)
+print(cnt)
