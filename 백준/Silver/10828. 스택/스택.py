23952143c13fb8d5feb192dd4838d7847ce4e8bc
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2024-03-21 15:19:50 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -10,21 +10,10 @@ for _ in range(N):
     if a[0] == 'push':
         stack.append(a[1])
     elif a[0] == 'pop':
-        if len(stack) == 0:
-            print(-1)
-        else:
-            print(stack.pop())
+        print(-1 ) if len(stack) == 0 else print(stack.pop())
     elif a[0] == 'size':
         print(len(stack))
     elif a[0] == 'empty':
-        if len(stack) == 0:
-            print(1)
-        else:
-            print(0)
+        print(1) if len(stack) == 0 else print(0)
     else:
-        if len(stack) == 0:
-            print(-1)
-        else:
-            print(stack[-1])
-
-
+        print(-1) if len(stack) == 0 else print(stack[-1])
