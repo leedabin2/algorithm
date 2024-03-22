8163fbec78af7b31240424293e1246740eb6d447
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2024-03-22 19:58:27 +0900

    [Silver II] Title: 최대 힙, Time: 124 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -3,13 +3,9 @@ import sys,heapq
 N = int(sys.stdin.readline().rstrip())
 
 h = []
-res = []
 for _ in range(N):
     x = int(sys.stdin.readline().rstrip())
     if x != 0:
         heapq.heappush(h,-x)
     else:
-        if h:
-            print(-heapq.heappop(h))
-        else:
-            print(0)
+        print(-heapq.heappop(h)) if h else print(0)
