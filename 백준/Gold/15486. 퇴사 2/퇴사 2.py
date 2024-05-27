789d2458bb3cf06fe47ffe700596c1a9799b9deb
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2024-05-27 11:02:41 +0900

    [Gold V] Title: 퇴사 2, Time: 3152 ms, Memory: 346452 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+sangdam = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+
+dp = [0 for _ in range(N+1)]
+for idx in range(N)[::-1]:
+    if idx + sangdam[idx][0] > N:
+        dp[idx] = dp[idx+1]
+    else:
+        dp[idx] = max(dp[idx+sangdam[idx][0]] + sangdam[idx][1], dp[idx + 1])
+
+print(max(dp))
