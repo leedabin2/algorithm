2d8fc407e34e1e9c6e7970610e1394765ba1ecdd
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2024-06-21 10:35:01 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,35 +1,26 @@
 import sys
 
 N = int(sys.stdin.readline().rstrip())
+M = int(sys.stdin.readline().rstrip())
 
 graph = [[] for _ in range(N+1)]
 
-computer_cnt = int(sys.stdin.readline().rstrip())
-
-visited = [0] * (N+1)
-
-def dfs(graph, v, visited):
-    visited[v] = 1
-
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph,i,visited)
-
-for _ in range(computer_cnt):
-    a,b = map(int,sys.stdin.readline().split())
+for _ in range(M):
+    a, b = map(int,sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
 
-dfs(graph, 1, visited)
 
-cnt = 0
-for i in visited:
-    if i == 1:
-        cnt += 1
+visitied = [0] * (N+1)
 
-print(cnt-1)
+def dfs(n):
+    visitied[n] = 1
 
+    for nxt in graph[n]:
+        if not visitied[nxt]:
+            dfs(nxt)
 
-
+dfs(1)
+print(sum(visitied)-1)
 
 
