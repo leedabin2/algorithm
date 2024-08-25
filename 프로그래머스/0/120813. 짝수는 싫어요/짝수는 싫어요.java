524bce1d29ed310572f7762aa1439c6ffcfb1846
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2024-08-25 23:05:39 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.69 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = 0 ; i <= n; i++) {
+            if ( i % 2 == 1) {
+                answer.add(i);
+            }
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
