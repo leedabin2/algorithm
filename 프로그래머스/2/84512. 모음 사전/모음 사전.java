0d3eb7514b9c48b655da537a914000f32173f7ee
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2024-10-28 16:16:39 +0900

    [level 2] Title: 모음 사전, Time: 19.31 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,24 @@
 import java.util.*;
-
 class Solution {
-    char[] alphabet = {'A', 'E', 'I', 'O', 'U'};
-    List<String> answer = new ArrayList<>();
+    List<String> res = new ArrayList<>();
     public int solution(String word) {
-        
-        recur("");
-       
-        return answer.indexOf(word);
-    }
+        int answer = 0;
     
-     public void recur(String curr) {
-         
-         answer.add(curr);
-         
-         if (curr.length() < 5) {
-             for (char a : alphabet) {
-                 recur(curr + a);
-             }
-         }
-            
+        recur("");
+        Collections.sort(res);
+        answer = res.indexOf(word);
+     
+        return answer;
+    }
+    private void recur(String curr) {
+        String[] arr = {"A", "E", "I", "O", "U"};
+        
+        if (curr.length() > 5) {
+            return;
+        }
+        res.add(curr);
+        for (String a : arr) {
+            recur(curr+a);
+        }
     }
 }
