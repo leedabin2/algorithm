b5201d9c6c8e2688cb36d348b7782eed461d0079
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2024-10-29 23:46:23 +0900

    [level 3] Title: 여행경로, Time: 1.02 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,49 +2,49 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[][] tickets) {
-        List<String> result = new ArrayList<>();
-     
-     
-        HashMap<String, Map<String, Integer>> ticket = new HashMap<>();
-        for (String[] t : tickets) {
-            String a = t[0];
-            String b = t[1];
-            ticket.computeIfAbsent(a, k -> new TreeMap<>()).put(b, ticket.getOrDefault(a, new HashMap<>()).getOrDefault(b, 0) + 1);
+        String[] answer = {};
+        Map<String,Map<String,Integer>> map = new HashMap<>();
+        for (String[] ticket : tickets) {
+            String a = ticket[0];
+            String b = ticket[1];
+            map.computeIfAbsent(a, k -> new TreeMap<>())
+                .put(b, map.get(a).getOrDefault(b,0) + 1);
         }
 
-        
-        dfs("ICN", ticket, result, tickets.length + 1);
-        
-      
+        List<String> result = new ArrayList<>();
+        dfs("ICN",result,map,tickets.length+1);
         return result.toArray(new String[0]);
     }
-    
-    public boolean dfs(String current, HashMap<String, Map<String, Integer>> ticket, List<String> result, int totalTickets) {
-        result.add(current);
-        
-        if (result.size() == totalTickets) {
-            return true; 
+    private boolean dfs(String start, List<String> result, Map<String,Map<String,Integer>> map, int total) {
+        result.add(start);
+        if (result.size() == total) {
+            return true;
         }
-
+        
+         if (!map.containsKey(start)) {
+            result.remove(result.size() - 1);
+            return false;
+        }
+        for (Map.Entry<String,Integer> entry : map.get(start).entrySet()) {
      
-        if (ticket.containsKey(current)) {
-            for (Map.Entry<String, Integer> entry : ticket.get(current).entrySet()) {
-                String next = entry.getKey();
-                int count = entry.getValue();
-
-                if (count > 0) { 
-                    ticket.get(current).put(next, count - 1); 
-                    if (dfs(next, ticket, result, totalTickets)) {
-                        return true; 
-                    } else {
-                    ticket.get(current).put(next, count);
-                    }
+            String next = entry.getKey();
+            Integer count = entry.getValue();
+            
+            
+            if (count > 0) {
+                map.get(start).put(next,count-1);
+                if (dfs(next,result,map,total)) {
+                    return true;
+                } else {
+                    map.get(start).put(next,count);
                 }
+                     
             }
         }
-
-    
-        result.remove(result.size() - 1);
+        
+         result.remove(result.size() - 1); 
+   
         return false;
+        
     }
 }
