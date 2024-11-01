7bb21d1e582e259b52cb528f7ad189457fb2c33e
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2024-11-01 19:11:07 +0900

    [level 3] Title: 단어 변환, Time: 0.37 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,57 +1,46 @@
 import java.util.*;
+
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int check = 0;
-        for (String word : words) {
-            if (word.equals(target)) {
-                check++;
-            }
-            continue;   
-        }
-      
-        if (check == 0) {
+        int answer = 0;
+        
+        if (!Arrays.toString(words).contains(target)) {
             return 0;
         }
         
-        boolean[] visited = new boolean[words.length];  
-          int answer = 0;
         Deque<String> q = new ArrayDeque<>();
         q.offer(begin);
-         while (!q.isEmpty()) {
-            int size = q.size();  
-            for (int i = 0; i < size; i++) {
+        boolean[] visited = new boolean[words.length];
+        while(!q.isEmpty()) {
+           
+            for (int i = 0; i < q.size(); i++) {
                 String curr = q.poll();
-                
-                if (curr.equals(target)) {
+                if(curr.equals(target)) {
                     return answer;
                 }
-
-                for (int j = 0; j < words.length; j++) {
-                    if (!visited[j] && checkOne(curr, words[j])) {
-                        visited[j] = true; 
-                        q.offer(words[j]);  
+                for (int j = 0 ; j < words.length; j++) {
+                    if (!visited[j] && check(curr,words[j])) {
+                        visited[j] = true;
+                        q.offer(words[j]);
                     }
                 }
+                
             }
-            answer++; 
+            answer++;
+         
         }
-        
         return answer;
     }
+    private boolean check(String word1, String word2) {
     
-    private boolean checkOne(String word1, String word2){
-        int same = 0;
+        int diff = 0;
         for (int i = 0; i < word1.length(); i++) {
-            if (word1.charAt(i) != (word2.charAt(i))) {
-                same++;
+            if (word1.charAt(i) != word2.charAt(i)) {
+                diff++;
             }
-            continue;
         }
         
-        if(same == 1) {
-            return true;
-        }
+        if (diff == 1) return true;
         return false;
     }
-    
 }
