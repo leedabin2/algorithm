07f35ad3f6efca6e28c96cf87be67972d13fb925
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-04-17 16:25:52 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 624 ms, Memory: 66068 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,34 +1,29 @@
-import sys,queue
-from collections import deque
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
-N, M =map(int,sys.stdin.readline().split())
+N, M = map(int,input().split())
 
-graph = [[]*(N+1) for _ in range(N+1)]
+graph = [[] for _ in range(N+1)]
 
-for _ in range(M):
-    a,b = map(int,sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
+for i in range(M):
+    u, v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
+count = 0
 visited = [0] * (N+1)
+def dfs(v):
+    visited[v] = 1
+    for n in graph[v]:
+        if not visited[n]:
+            dfs(n)
 
-def bfs(s):
-    q = deque([s])
-    while q:
-        node = q.popleft()
-        visited[node] = 1
+for i in range(1, N+1):
+    if not visited[i]:
+        dfs(i)
+        count += 1
 
-        for nxt in graph[node]:
-            if not visited[nxt]:
-                q.append(nxt)
-                visited[nxt] = 1
+print(count)
 
-# bfs(1)
 
-cnt = 0
-for i in range(1,N+1):
-    if visited[i] == 0:
-        bfs(i)
-        cnt += 1
-
-print(cnt)
