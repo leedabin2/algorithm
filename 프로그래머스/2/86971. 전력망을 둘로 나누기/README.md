7cc7ca10b937b8f7cece0f575054abf28411fcc0
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-04-19 16:40:46 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.30 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 9.16 MB, 시간: 7.95 ms
+메모리: 9.11 MB, 시간: 4.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 17일 17:57:45
+2025년 04월 19일 16:40:36
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,34 +1,31 @@
 from collections import deque
-def bfs(start,graph,n,visited):
+def bfs(start,graph,visited):
     count = 1
     queue = deque([start])
     visited[start] = 1
     
     while queue:
-        nxt_node = queue.popleft()
-        for n in graph[nxt_node]:
-            if not visited[n]:
-                visited[n] = 1
-                queue.append(n)
+        node = queue.popleft()
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                visited[next_node] = 1
+                queue.append(next_node)
                 count += 1
+    
     return count
-        
 def solution(n, wires):
-    min_diff = n
+    answer = n
     for i in range(len(wires)):
         graph = [[] for _ in range(n+1)]
         for j in range(len(wires)):
             if i == j:
                 continue
             a, b = wires[j]
-            
             graph[a].append(b)
-            graph[b].append(a)     
-    
+            graph[b].append(a)
         visited = [0] * (n+1)
-        graph_count = bfs(1,graph,n,visited)
-        diff = abs((n - graph_count) - graph_count)
-    
-        min_diff = min(diff,min_diff)
-    
-    return min_diff
+        group_count = bfs(1,graph,visited)
+        diff = abs((n - (group_count)) - group_count)
+        answer = min(answer,diff)
+
+    return answer
