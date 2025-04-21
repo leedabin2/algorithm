b80de021d4401162650a068f5363e6579a485d58
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-04-21 13:12:31 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,19 +1,27 @@
 import sys
 
 N = int(sys.stdin.readline().rstrip())
-
 stack = []
-
 for _ in range(N):
-    a = list(sys.stdin.readline().split())
-
-    if a[0] == 'push':
-        stack.append(a[1])
-    elif a[0] == 'pop':
-        print(-1 ) if len(stack) == 0 else print(stack.pop())
-    elif a[0] == 'size':
+    cmd = sys.stdin.readline().split()
+    if cmd[0] == 'push':
+        x = int(cmd[1])
+        stack.append(x)
+    if cmd[0] == 'pop':
+        if not stack:
+            print(-1)
+        else:
+            print(stack.pop())
+    if cmd[0] == 'size':
         print(len(stack))
-    elif a[0] == 'empty':
-        print(1) if len(stack) == 0 else print(0)
-    else:
-        print(-1) if len(stack) == 0 else print(stack[-1])
+    if cmd[0] == 'empty':
+        if not stack:
+            print(1)
+        else:
+            print(0)
+    if cmd[0] == 'top':
+        if not stack:
+            print(-1)
+        else:
+            print(stack[-1])
+
