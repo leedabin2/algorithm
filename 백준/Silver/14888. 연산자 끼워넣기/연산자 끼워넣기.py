f7432c926a1a9a22651d2882263d9f03a994c33c
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-04-21 16:36:21 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 48 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,38 +1,35 @@
 import sys
-from collections import deque
 
 N = int(sys.stdin.readline().rstrip())
 A = list(map(int,sys.stdin.readline().split()))
 plus, minus, mul, div = map(int,sys.stdin.readline().split())
 
-queue = deque()
-queue.append([A[0],plus,minus,mul,div,1])
-
 max_res = -10000000000
 min_res = 10000000000
 
-while queue:
-    num, plus, minus, mul, div, depth = queue.popleft()
+def dfs(idx,num,plus,minus,mul,div):
+    global max_res,min_res
 
-    if depth == N:
+    if idx == N:
         max_res = max(max_res,num)
         min_res = min(min_res,num)
-        continue
-
-    next_num = A[depth]
+        return
+    nxt_num = A[idx]
 
     if plus > 0:
-        queue.append([num+next_num, plus-1,minus,mul,div,depth+1])
+        dfs(idx+1,num+nxt_num,plus-1,minus,mul,div)
     if minus > 0:
-        queue.append([num-next_num,plus,minus-1,mul,div,depth+1])
+        dfs(idx+1,num-nxt_num,plus,minus-1,mul,div)
     if mul > 0:
-        queue.append([num * next_num, plus, minus, mul-1, div, depth + 1])
+        dfs(idx+1,num*nxt_num,plus,minus,mul-1,div)
     if div > 0:
-        if num < 0 and next_num > 0:
-            change_num = -(abs(num) // next_num)
-            queue.append([change_num, plus, minus, mul, div-1, depth + 1])
+        if num < 0 and nxt_num > 0:
+            change_num = -(abs(num)//nxt_num)
+            dfs(idx + 1, change_num, plus, minus, mul, div - 1)
         else:
-            queue.append([num // next_num, plus, minus, mul, div-1, depth + 1])
+            dfs(idx+1,num//nxt_num,plus,minus,mul,div-1)
 
+
+dfs(1,A[0],plus,minus,mul,div)
 print(max_res)
 print(min_res)
