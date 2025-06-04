08f3106432e7af4a840c223585949e39eaf008cc
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-06-04 17:45:55 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 16.17 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,30 +2,51 @@ import java.util.*;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> answer = new ArrayList<>();
-        // 현재날짜로부터 (28*12)+17을 계산 후 6달보다 높으면 result +
-        HashMap<String,Integer> hashTerms = new HashMap<>();
-        for (int i = 0; i < terms.length; i++) {
-            String[] a = terms[i].split(" ");
-            hashTerms.put(a[0],Integer.parseInt(a[1]));
+        
+        Map<String,Integer> term = new HashMap<>();
+        for (String t : terms) {
+            String[] row = t.split(" ");
+            term.put(row[0], Integer.parseInt(row[1]));
         }
-      
-        String[] todaysParse = today.split("\\.");
-        int year = Integer.parseInt(todaysParse[0]);
-        int month = Integer.parseInt(todaysParse[1]);
-        int day = Integer.parseInt(todaysParse[2]);
         
         for (int i = 0; i < privacies.length; i++) {
-            String[] parsePrivacies = privacies[i].split(" ");
-            String pTerm = parsePrivacies[1];
-            int pYear =  Integer.parseInt(parsePrivacies[0].split("\\.")[0]);
-            int pMonth =  Integer.parseInt(parsePrivacies[0].split("\\.")[1]);
-            int pDay =  Integer.parseInt(parsePrivacies[0].split("\\.")[2]);
+            String[] privacy = privacies[i].split(" ");
+            String[] date = privacy[0].split("\\.");
             
-            int total = ((year-pYear)*12*28 + (month-pMonth)*28 + (day-pDay));
-            if(total >= hashTerms.get(pTerm) * 28) {
+            int year = Integer.parseInt(date[0]);
+            int month = Integer.parseInt(date[1]);
+            int day =  Integer.parseInt(date[2]);
+            String type = privacy[1];
+            
+            int typeMonth = term.get(type);
+            
+            boolean expire = checkExprire(today, year,month,day,typeMonth);
+            
+            if (expire) {
                 answer.add(i+1);
             }
+            
+            
         }
-        return answer.stream().mapToInt(i -> i).toArray();
+        return answer.stream().mapToInt(x -> x).toArray();
+    }
+    private boolean checkExprire(String today, int year, int month, int day, int typeMonth) {
+        String[] todays = today.split("\\.");
+        
+        int todayYear = Integer.parseInt(todays[0]);
+        int todayMonth = Integer.parseInt(todays[1]);
+        int todayDay = Integer.parseInt(todays[2]);
+        
+        int todayTotal = todayYear * 12 * 28 + todayMonth * 28 + todayDay;
+        int startTotal = year * 12 * 28 + month * 28 + day;
+
+        
+        int expireDate = startTotal + (typeMonth * 28);
+        if (expireDate <= todayTotal) {
+            return true;
+        }
+        
+        return false;
+        
     }
 }
