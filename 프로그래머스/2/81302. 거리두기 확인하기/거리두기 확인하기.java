ddd0e6227eaaaa42dd90c3ecfd23a1909f1fdd3b
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-06-10 13:04:28 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.60 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -4,63 +4,54 @@ class Solution {
     public int[] solution(String[][] places) {
         int[] answer = new int[5];
         
-        // 5개의 대기실에 대해 검사
-        for (int i = 0; i < 5; i++) {
-            boolean isSafe = true; 
+        for (int idx = 0; idx < 5; idx++) {
+            String[] place = places[idx];
             
-         
-            for (int x = 0; x < 5 && isSafe; x++) {
-                for (int y = 0; y < 5 && isSafe; y++) {
-                    if (places[i][x].charAt(y) == 'P') {
-                       
-                        if (!check(x, y, places[i])) {
-                            isSafe = false;  
+            boolean ok = true;
+            
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (place[i].charAt(j) == 'P') {
+                        if(!isSafe(i,j,place)) {
+                            ok = false;
                         }
                     }
                 }
             }
-            answer[i] = isSafe ? 1 : 0;  
+            
+            if (ok) answer[idx] = 1;            
+            else answer[idx] = 0;
         }
         return answer;
     }
     
-
-    private boolean check(int x, int y, String[] places) {
-     
-        int[][] dist = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private static boolean isSafe(int x, int y, String[] place) {
+        int[][] directions = new int[][]{{-1,0},{0,-1},{1,0},{0,1}};
+        boolean[][] visited = new boolean[5][5];
+        Deque<int[]> peoples = new ArrayDeque<>();
+        peoples.add(new int[]{x,y,0});
+        visited[x][y] = true;
         
-        // 1칸 거리 확인
-        for (int[] d : dist) {
-            int nextX = x + d[0];
-            int nextY = y + d[1];
+        while(!peoples.isEmpty()) {
+            int[] curr = peoples.poll();
+            int cx = curr[0], cy = curr[1], dist = curr[2];
             
-          
-            if (nextX >= 0 && nextX < 5 && nextY >= 0 && nextY < 5) {
-             
-                if (places[nextX].charAt(nextY) == 'P') {
-                    return false;
-                }
-                
-                // 1칸 거리가 빈 자리일 경우, 2칸 거리 확인
-                if (places[nextX].charAt(nextY) == 'O') {
-                   
-                    for (int[] dd : dist) {
-                        int nx = nextX + dd[0];
-                        int ny = nextY + dd[1];
-                        
-                    
-                        if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5) {
-                           
-                            if (nx == x && ny == y) continue;
-                         
-                            if (places[nx].charAt(ny) == 'P') {
-                                return false;
-                            }
-                        }
+            if (dist >= 1 && place[cx].charAt(cy) == 'P') return false;
+            if (dist == 2) continue;
+            
+            for (int[] dir : directions) {
+                int nx = dir[0] + cx, ny = dir[1] + cy;
+                if (0 <= nx && nx < 5 && 0 <= ny && ny < 5 && !visited[nx][ny]) {
+                    if (place[nx].charAt(ny) != 'X') {
+                        visited[nx][ny] = true;
+                        peoples.add(new int[]{nx,ny,dist+1});
                     }
                 }
             }
+            
         }
+        
         return true;
+        
     }
 }
