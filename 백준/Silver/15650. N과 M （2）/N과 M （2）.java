26f4a6c6bdceb504fe1c7223d76dcd673301bb1b
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-08-11 15:50:50 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -3,32 +3,39 @@ import java.io.*;
 
 class Main {
   static int N, M;
-  static int[] result;
   public static void main(String[] args) throws IOException {
     BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
 
     String[] st = br.readLine().split(" ");
-
     N = Integer.parseInt(st[0]);
-    M = Integer.parseInt(st[1]);
+   M = Integer.parseInt(st[1]);
 
-    result = new int[M];
-    dfs(0, 1);
+    int[] arr = new int[N];
+
+    for (int i = 1; i <= N; i++) {
+      arr[i-1] = i;
+    }
+    List<Integer> unique = new ArrayList<>();
+
+    find(unique, 0, arr);
   }
+  private static void find(List<Integer> unique, int idx, int[] arr) {
+    if (M == unique.size()) {
+      for (int i =0; i < unique.size(); i++) {
+        System.out.print(unique.get(i));  
+        if (i != M - 1) System.out.print(" ");
 
-  static void dfs(int depth, int start) {
-    if (depth == M) {
-      for (int i = 0; i < M; i++) {
-        System.out.print(result[i] + " ");
       }
       System.out.println();
       return;
     }
 
-    for (int i = start; i <= N; i++) {
-      result[depth] = i; 
-      dfs(depth + 1, i + 1);
+    for (int i = idx; i < N; i++) {
+      if (!unique.contains(arr[i])) {
+        unique.add(arr[i]);
+        find(unique, i + 1, arr);
+        unique.remove(unique.size()-1);
+      }
     }
   }
-
 } 
