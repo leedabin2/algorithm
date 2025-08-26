e5976dc2a8b08b80dbe23b74e4d875943a8a2254
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-08-26 23:00:40 +0900

    [Gold V] Title: 경쟁적 전염, Time: 216 ms, Memory: 20448 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -2,19 +2,23 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+  static int[][] directions = new int[][]{{-1,0},{0, 1}, {1,0}, {0,-1}};
   public static void main(String[] args) throws IOException {
     BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+
+    // 1 
     String[] st = br.readLine().split(" ");
     int N = Integer.parseInt(st[0]);
     int K = Integer.parseInt(st[1]);
 
     int[][] board = new int[N][N];
-    int[][] directions = new int[][]{{-1,0},{0,-1},{1,0},{0,1}};
+    List<int[]> virusList = new ArrayList<>();
 
     for (int i = 0; i < N; i++) {
       String[] row = br.readLine().split(" ");
       for (int j = 0; j < N; j++) {
         board[i][j] = Integer.parseInt(row[j]);
+        if (board[i][j] > 0) virusList.add(new int[]{board[i][j], i, j});
       }
     }
 
@@ -23,37 +27,41 @@ class Main {
     int X = Integer.parseInt(input[1]) - 1;
     int Y = Integer.parseInt(input[2]) - 1;
 
-    List<int[]> viruses = new ArrayList<>();
+
+    Collections.sort(virusList, (a, b) -> Integer.compare(a[0], b[0]));
+
+
     Deque<int[]> queue = new ArrayDeque<>();
-    
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        if (board[i][j] != 0) viruses.add(new int[]{board[i][j],0,i,j});
-      }
+
+    for (int[] v : virusList) {
+      queue.add(v);
     }
+    
+    int time = 0;
+    while(!queue.isEmpty() && time < S) {
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        int[] curr = queue.poll();
 
-    viruses.sort(Comparator.comparingInt(a -> a[0]));
-    for (int[] virus : viruses) queue.add(virus);
+        int virus = curr[0], x = curr[1], y = curr[2];
+        for (int[] d: directions) {
+          int nx =d[0] + x, ny = d[1] + y;
 
-    while(!queue.isEmpty()) {
-      int[] curr = queue.poll();
-
-      int virus = curr[0],time = curr[1], cx = curr[2], cy = curr[3];
-
-      if (time == S) break;
-
-      for (int[] dir: directions) {
-        int nx = dir[0] + cx, ny = dir[1] + cy;
-        if ( 0 <= nx && nx < N && 0 <= ny && ny < N) {
-          if (board[nx][ny] == 0) {
-            board[nx][ny] = virus;
-            queue.add(new int[]{virus, time+1, nx, ny});
+          if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+            if (board[nx][ny] == 0) {
+              board[nx][ny] = virus;
+              queue.add(new int[]{virus, nx, ny});
+            }
           }
         }
       }
 
+      time++;
+
+      
     }
 
-    System.out.println(board[X][Y] == 0 ? 0 : board[X][Y]);
+    System.out.println(board[X][Y] > 0 ? board[X][Y] : 0);
+
   }
-} 
+}
