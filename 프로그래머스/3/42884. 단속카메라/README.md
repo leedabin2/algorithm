202e4132c2ed9232039c07cf21a69f84dbca5efa
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-09-07 13:14:33 +0900

    [level 3] Title: 단속카메라, Time: 10.13 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=java) 
 
 ### 성능 요약
 
-메모리: 54.4 MB, 시간: 8.84 ms
+메모리: 60.6 MB, 시간: 10.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 22일 18:13:08
+2025년 09월 07일 13:14:29
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,18 +1,27 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
+        
+        // 진출이 작은 것부터 
+        Arrays.sort(routes, (a, b) -> {
+            if (a[1] != b[1]) return a[1] - b[1];
+            return a[0] - b[0];
+        });
+         
+        int camera = routes[0][1];
+        int cnt = 1;
 
-        Arrays.sort(routes, Comparator.comparingInt(a->a[1]));
-        int cameraPosition = -9999999;
-        
-        for (int[] r : routes) {
-            if (cameraPosition < r[0]) {
-                cameraPosition = r[1];
-                answer++;
+        for (int i = 1; i < routes.length; i++) {
+            if (camera < routes[i][0]) {
+                // 새 카메라 필요
+                camera = routes[i][1];
+                cnt++;
             }
+            
         }
-        
-        return answer;
+            
+        return cnt;
     }
 }
