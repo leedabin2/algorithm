cedf0b058c12d068a48139c42cf79a273ebda734
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-11-21 11:20:40 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 660 ms, Memory: 50320 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
@@ -1,16 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 class Main {
   static final int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
   static final int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
-  static List<List<List<Integer>>> board;
-  static Deque<int[]> balls;
+  static List<List<List<Integer>>> board = new ArrayList<>();
+  static List<int[]> balls = new ArrayList<>();
   static int N, M, K;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,45 +15,43 @@ class Main {
     M = Integer.parseInt(st[1]);
     K = Integer.parseInt(st[2]);
 
-    board = new ArrayList<>();
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++){
       board.add(new ArrayList<>());
-      for (int j = 0; j < N; j++) board.get(i).add(new ArrayList<>());
+      for (int j = 0; j < N; j++) {
+        board.get(i).add(new ArrayList<>());
+      }
     }
 
-    balls = new ArrayDeque<>();
-    for (int id = 0; id < M; id++) {
-      String[] row = br.readLine().split(" ");
-      int r = Integer.parseInt(row[0]) - 1;
-      int c = Integer.parseInt(row[1]) - 1;
-      int m = Integer.parseInt(row[2]);
-      int s = Integer.parseInt(row[3]);
-      int d = Integer.parseInt(row[4]);
-      balls.add(new int[]{r, c, m, s, d});
+    for (int i = 0; i < M; i++) {
+      String[] info  = br.readLine().split(" ");
+      int r = Integer.parseInt(info[0]);
+      int c = Integer.parseInt(info[1]);
+      int m = Integer.parseInt(info[2]);
+      int s = Integer.parseInt(info[3]);
+      int d = Integer.parseInt(info[4]);
 
+      balls.add(new int[]{r, c, m, s, d});
     }
 
     while (K-- > 0) {
-      moveFireBall();
+      moveFireBalls();
       mergeFireBalls();
-
+      
     }
-
-     System.out.println(sumMass());
+    System.out.println(sumMass());
+    
   }
-  static void moveFireBall() {
-
-    for (int i = 0; i < N; i++) {
+  static void moveFireBalls() {
+    for (int i =0; i < N; i++) {
       for (int j = 0; j < N; j++) {
         board.get(i).get(j).clear();
       }
     }
 
-    Deque<int[]> newBalls = new ArrayDeque<>();
-    
-    int size = balls.size();
-    for (int id =0; id < size; id++) {
-      int[] b  = balls.poll();
+    List<int[]> newBalls = new ArrayList<>();
+
+    for (int i = 0; i < balls.size(); i++) {
+      int[] b = balls.get(i);
 
       int r = b[0];
       int c = b[1];
@@ -69,68 +62,66 @@ class Main {
       int nr = (r + dx[d] * (s % N) + N) % N;
       int nc = (c + dy[d] * (s % N) + N) % N;
 
-      board.get(nr).get(nc).add(newBalls.size()); // idx 저장
       newBalls.add(new int[]{nr, nc, m, s, d});
+      board.get(nr).get(nc).add(i);
+
     }
     balls = newBalls;
+
   }
-  static void mergeFireBalls(){
-    Deque<int[]> newBalls = new ArrayDeque<>();
+  static void mergeFireBalls() {
+    List<int[]> newBalls = new ArrayList<>();
     for (int r = 0; r < N; r++) {
       for (int c = 0; c < N; c++) {
+
         List<Integer> cell = board.get(r).get(c);
-  
+
         if (cell.size() == 0) continue;
 
         if (cell.size() == 1) {
-            int idx = cell.get(0);
-            int[][] tempBalls = balls.toArray(new int[0][]);
-            newBalls.add(tempBalls[idx]);
-            continue;
+          int[] b = balls.get(cell.get(0));
+          newBalls.add(b);
+          continue;
         }
-       
-          int[][] tempBalls = balls.toArray(new int[0][]);
 
-          int mass = 0;
-          int speed = 0;
-          boolean allEven = true;
-          boolean allOdd = true;
+        int mass = 0;
+        int speed = 0;
+        boolean allAdd = true;
+        boolean allEven = true;
 
-          for (int idx : cell) {
-            int[] b = tempBalls[idx];
+        for (int id : cell) {
+          int[] b = balls.get(id);
+          
+          int m = b[2];
+          int s = b[3];
+          int d = b[4];
 
-            int m = b[2];
-            int s = b[3];
-            int d= b[4];
+          mass += m;
+          speed += s;
 
-            mass += m;
-            speed += s;
-            if (d % 2 == 0) allOdd = false;
-            else  allEven = false;
-           
-   
-          }
+          if (d % 2 == 0) {
+            allAdd = false;
+          } else allEven = false;
 
-          int newMass = (mass/5);
-          if (newMass == 0) continue; 
-          int newSpeed = (speed/cell.size());
+        }
 
-          int[] dirs = (allEven || allOdd) ? new int[]{0,2,4,6} : new int[]{1,3,5,7};
+        int newMass = (mass/5);
+        if (newMass == 0) continue;
+        int newSpeed = (speed/cell.size());
 
-          for (int d : dirs) {
-              newBalls.add(new int[]{r, c, newMass, newSpeed, d});
-          }
-        
+        int[] dirs = (allAdd || allEven) ? new int[]{0, 2, 4, 6} : new int[]{1, 3, 5,7};
+
+        for (int d : dirs) {
+          newBalls.add(new int[]{r, c, newMass, newSpeed, d});
+        }
       }
-      
     }
+
     balls = newBalls;
   }
   static int sumMass() {
     int sum = 0;
-    while (!balls.isEmpty()) {
-      int[] b = balls.poll();
-
+    for (int[] b : balls) {
       int m = b[2];
       sum += m;
     }
