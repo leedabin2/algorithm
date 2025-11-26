7134ebac7f0b4f47ea61f73b3e311a9caca212f3
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-11-26 14:52:15 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,63 +2,61 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int N, dangi;
-    static int[][] board;
-    static boolean[][] visited;
-    static int[][] directions;
-    public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      String st = br.readLine();
-
-      N = Integer.parseInt(st);
-
-      board = new int[N][N];
-
-      for (int i = 0; i < N; i++) {
-        String[] line = br.readLine().split("");
-        for (int j = 0 ; j < N ; j++) {
-          board[i][j] = Integer.parseInt(line[j]);
-        }
-      }
-    visited = new boolean[N][N];
-    directions = new int[][]{{-1,0},{0,1},{1,0}, {0,-1}};
-    List<Integer> dangiSizes = new ArrayList<>();
-
-    for (int i =0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        if (board[i][j] != 0 && !visited[i][j]) {
-          dangi = 1;
-          dfs(i,j);
-          dangiSizes.add(dangi);
-        }
-      }
-    }
-    Collections.sort(dangiSizes);
-    System.out.println(dangiSizes.size());
-    for (int size : dangiSizes) {
-      System.out.println(size);
-    }
+  static int n;
+  static int[][] board;
+  static boolean[][] visited;
+  static int[] dx= {0, 0, -1, 1};
+  static int[] dy = {1, -1, 0 ,0};
+  public static void main (String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       
+    n = Integer.parseInt(br.readLine());
+
+    board = new int[n][n];
+
+    for (int i = 0; i < n; i++) {
+      String[] row = br.readLine().split("");
+      for (int j = 0; j < n; j++) {
+        board[i][j] = Integer.parseInt(row[j]);
+      }
     }
 
-    private static void dfs(int x, int y) {
-      visited[x][y] = true;
+    List<Integer> result = new ArrayList<>();
+    visited = new boolean[n][n];
 
-      for ( int[] dir : directions) {
-        int dx = dir[0];
-        int dy = dir[1];
 
-        int nx = x + dx;
-        int ny = y + dy;
-
-        if (0<= nx && nx < N && 0<= ny && ny < N && board[nx][ny] == 1) {
-          if (!visited[nx][ny]) {
-            visited[nx][ny] = true;
-            dangi++;
-            dfs(nx,ny);
-        
-          }
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (!visited[i][j] && board[i][j] == 1) {
+          int houses = dfs(i, j); //단지내 집의 수
+          result.add(houses);
         }
       }
     }
-} 
+
+    Collections.sort(result);
+    System.out.println(result.size());
+    for (int i = 0; i < result.size(); i++) {
+      System.out.println(result.get(i));
+    }
+
+  }
+  static int dfs(int x, int y) {
+    int cnt = 1;
+    visited[x][y] = true;
+
+    for (int i = 0; i < 4; i++) {
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+
+      if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+
+      if (board[nx][ny] == 1 && !visited[nx][ny]) {
+        cnt += dfs(nx, ny);
+      }
+    }
+
+    return cnt;
+  }
+  
+}
