6f0706756f90b0df76d2f042ea84358e22943beb
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-11-27 21:59:43 +0900

    [Gold V] Title: 토마토, Time: 724 ms, Memory: 125160 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,76 +1,71 @@
-import java.util.*;
 import java.io.*;
-// 4: 15 -> 4 :54
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Main {
   static int N, M;
-  static int[][] board;
-  static boolean[][] visited;
-  static int[][] directions;
-  static Deque<int[]> queue;
-  public static void main(String[] args) throws IOException  {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] st = br.readLine().split(" ");
-    
-    N = Integer.parseInt(st[0]);
-    M = Integer.parseInt(st[1]);
-    board = new int[M][N];
-    for (int i = 0 ; i < M ; i++) {
-      String[] line = br.readLine().split(" ");
-      for (int j = 0; j < N; j++ ) {
-        board[i][j] = Integer.parseInt(line[j]);
-      }
-    }
-    
-    queue = new ArrayDeque<>();
-    visited = new boolean[M][N];
-    directions = new int[][] {{-1,0},{1,0},{0,1},{0,-1}};
-    for (int i = 0; i < M; i++) {
-      for (int j = 0; j < N; j++) {
-        if (board[i][j] == 1) {
-          queue.add(new int[]{i,j});
-          visited[i][j] = true;
-        }
-      }
-    }
+  static int[] dx = new int[]{0, 0, -1, 1};
+  static int[] dy = new int[]{1, -1, 0, 0};
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    bfs();
+      String[] st = br.readLine().split(" ");
+      N = Integer.parseInt(st[1]);
+      M = Integer.parseInt(st[0]);
 
-    int maxDay = 0;
-    for (int i = 0; i < M; i++) {
-      for (int j =0; j < N; j++) {
-        if (board[i][j] == 0) {
-          System.out.println(-1);
-          return;
-        }
-        else {
-          maxDay = Math.max(maxDay, board[i][j]);
-        }
-      }
-    }
+      int[][] board = new int[N][M];
+      Deque<int[]> q = new ArrayDeque<>();
+      boolean[][] visited = new boolean[N][M];
 
-     System.out.println(maxDay-1);
-  }
+      boolean isFull = true;
+      for (int i =0; i < N; i++) {
+        String[] row = br.readLine().split(" ");
+        for (int j = 0; j < M; j++) {
+          board[i][j] = Integer.parseInt(row[j]);
 
-  private static void bfs() {
-    while (!queue.isEmpty()) {
-      int[] cur = queue.poll();
-      
-      int cx = cur[0];
-      int cy = cur[1];
-
-      for (int[] dir : directions) {
-        int nx = dir[0] + cx;
-        int ny = dir[1] + cy;
-
-        if (0<= nx && nx < M && 0<= ny && ny < N && board[nx][ny] == 0) {
-          if (!visited[nx][ny]) {
-            visited[nx][ny] = true;
-            board[nx][ny] = board[cx][cy] + 1;
-            queue.add(new int[]{nx,ny});
+          if (board[i][j] == 0) isFull = false;
+          if (board[i][j] == 1) {
+            q.add(new int[]{i, j});
+            visited[i][j] = true;
           }
         }
       }
-    }
-  }
+    
+      if (isFull) {
+        System.out.println(0); // 이미 토마토가 모두 익어있는 상태
+        return;
+      }
 
+      while (!q.isEmpty()) { 
+        int[] curr = q.poll();
+
+        int cx = curr[0], cy = curr[1];
+
+        for (int d = 0; d < 4; d++) {
+          int nx = dx[d] + cx, ny = dy[d] + cy;
+
+          if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+
+          if (!visited[nx][ny] && board[nx][ny] != -1) {
+            visited[nx][ny] = true;
+            board[nx][ny] = board[cx][cy] + 1;
+            q.add(new int[]{nx, ny});
+          }
+        }
+      }
+
+      boolean isOut = false;
+      int total = 0;
+
+      for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+          if (board[i][j] == 0) isOut = true;
+          total = Math.max(board[i][j], total);
+        }
+      
+      }
+
+      System.out.println(isOut ? -1 : total - 1);
+
+    }
 }
