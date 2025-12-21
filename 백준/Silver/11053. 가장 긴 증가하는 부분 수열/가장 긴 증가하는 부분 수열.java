579ec68efc95b239808232bc9ab7bce3610d77c7
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-12-21 12:57:26 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,33 +1,32 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int A = Integer.parseInt(br.readLine());
-        int[] lst = new int[A];
-        String[] st = br.readLine().split(" ");
-        for (int i = 0; i < A; i++) {
-            lst[i] = Integer.parseInt(st[i]);
-        }
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] dp = new int[A];
-        Arrays.fill(dp, 1); 
+      int N = Integer.parseInt(br.readLine());
+      String[] st = br.readLine().split(" ");
+      int[] a = new int[N];
 
-        for (int i = 1; i < A; i++) {
-          for (int j = 0; j < i; j++) {
-            if (lst[j] < lst[i]) {
-              dp[i] = Math.max(dp[i], dp[j]+1);
-            }
+      for (int i =0; i < N; i++) {
+        a[i] = Integer.parseInt(st[i]);
+      }
+
+      int[] dp = new int[N];
+      Arrays.fill(dp, 1);
+      int answer = dp[0];
+
+      for (int i = 1; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+          if (a[i] > a[j]) {
+            dp[i] = Math.max(dp[i], dp[j] + 1);
           }
         }
+        answer = Math.max(answer, dp[i]);
+      }
 
-        int max = 0;
-        for (int n : dp) {
-          if (n > max) {
-            max = n;
-          }
-        }
-        System.out.println(max);
-    }
+      System.out.println(answer);
+
+   }
 }
