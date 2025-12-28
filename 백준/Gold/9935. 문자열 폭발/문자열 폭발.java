682651b116edf5ff76dbbc5c327999039bec1932
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2025-12-28 13:21:46 +0900

    [Gold IV] Title: 문자열 폭발, Time: 352 ms, Memory: 25512 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -2,59 +2,40 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+  public static void main (String[] args) throws IOException{
 
-    // 1 : 55 - > 2 : 45
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    String[] input = br.readLine().split("");
+    String s = br.readLine();
+
     String bomb = br.readLine();
+    int len = bomb.length();
 
-    char[] b = bomb.toCharArray();
-    int len = input.length;
+    StringBuilder stack = new StringBuilder();
 
-    int m = bomb.length();
+    for (char ch : s.toCharArray()) {
 
-    Deque<Character> queue = new ArrayDeque<>();
-    for (int i = 0; i < len; i++) {
-      char ch = input[i].charAt(0);
-      queue.addLast(ch);
-      
-      if (b[m-1] == ch && queue.size() >= m) {
-        char[] buf = new char[m];
-        boolean isMatch = true;
-        for (int j = m-1; j >= 0; j--) {
-          char x = queue.removeLast();
-          buf[j] = x;
-          if (b[j] != x) {
-            isMatch = false;
+      stack.append(ch);
+
+      if (stack.length() >= len) {
+        boolean match = true;
+
+        for (int i = 0; i < len; i++) {
+          if (stack.charAt(stack.length()-len + i) != bomb.charAt(i)) {
+            match = false;
+            break;
           }
         }
 
-        if (!isMatch) {
-          for (int j = 0; j < m; j++) {
-            queue.addLast(buf[j]);
-          }
+        if (match) {
+          // 폭발
+          stack.delete(stack.length()-len, stack.length());
         }
-
-      
       }
-
-
     }
 
-
-    StringBuilder sb = new StringBuilder(queue.size());
-
-    if (queue.isEmpty()) {
-      System.out.println("FRULA");
-    } else {
-      while(!queue.isEmpty()) {
-      char curr = queue.poll();
-        sb.append(curr);
-      }
-      System.out.println(sb);
-    }
+    if (stack.length() == 0) System.out.println("FRULA");
+    else System.out.println(stack.toString());
 
   }
-} 
+}
