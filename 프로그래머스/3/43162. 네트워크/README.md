9bc3d4bb01f1c049acc37f8baea24b886b342550
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2026-01-03 12:42:12 +0900

    [level 3] Title: 네트워크, Time: 0.37 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?gad_source=1&gad_campaignid=22366107751&gbraid=0AAAAAC_c4nBEI1146nIhlEFJf3800AO9l&gclid=EAIaIQobChMIkvTy863ukQMVYWsPAh08QgysEAAYASAAEgKR8_D_BwE) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 0.31 ms
+메모리: 84.6 MB, 시간: 0.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 16:08:07
+2026년 01월 03일 12:42:08
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,37 +1,37 @@
 import java.util.*;
 
 class Solution {
-  static boolean[] visited;
-  
+    static boolean[] visited;
+    static Deque<Integer> queue;
     public int solution(int n, int[][] computers) {
-        int count = 0;
-
+        int answer = 0;
         visited = new boolean[n];
-
+        queue = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
-          if (!visited[i]) {
-            bfs(i,n,computers);
-            count++;
-          }
+            if (!visited[i]) {
+                bfs(i, computers);
+                answer++;
+            }
         }
-        return count;
+        
+        return answer;
     }
-  private static void bfs(int node, int n, int[][] computers) {
-    Deque<Integer> queue = new ArrayDeque<>();
-    queue.add(node);
-
-    visited[node] = true;
-
-    while (!queue.isEmpty()) {
-      int cur = queue.poll();
-      
-      for (int nxt = 0; nxt < n; nxt++) {
-        if (!visited[nxt] && computers[cur][nxt] == 1) {
-          visited[nxt] = true;
-          queue.add(nxt);
+    
+    static void bfs(int start, int[][] computers) {
+        
+        queue.add(start);
+        visited[start] = true;
+        while(!queue.isEmpty()) {
+            int curr = queue.poll();
+            
+            for (int next = 0 ; next < computers.length; next++) {        if (computers[curr][next] == 1 && !visited[next]) {
+                    visited[next] = true;
+                    queue.add(next);
+                }
+                
+            }
         }
-      }
+        
+        
     }
-
-  }
 }
