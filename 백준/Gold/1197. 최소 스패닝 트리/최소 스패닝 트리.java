60d0e859e863e41fa2d491759f2e7b9aeeb4f815
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2026-01-03 13:23:35 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 684 ms, Memory: 68056 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,61 +1,72 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-    Map<Integer, List<int[]>> graph  = new HashMap<>();
-
-    String[] st = br.readLine().split(" ");
-    int V = Integer.parseInt(st[0]);
-    int E = Integer.parseInt(st[1]);
-
-    for (int i = 1; i <= V; i++) {
-      graph.put(i, new ArrayList<>());
-    }
-
-    for (int i =0 ; i < E; i++) {
-      String[] row = br.readLine().split(" ");
-
-      int a = Integer.parseInt(row[0]);
-      int b = Integer.parseInt(row[1]);
-      int c = Integer.parseInt(row[2]);
-
-      graph.get(a).add(new int[]{b, c});
-      graph.get(b).add(new int[]{a, c});
-    }
-
-    boolean[] visited = new boolean[V+1];
-
-    PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-    pq.add(new int[]{0,1}); // cost, to
-
-    int total = 0;
-    int visitedCount = 0;
-    while(!pq.isEmpty()) {
-      int[] curr = pq.poll();
-      int cost = curr[0];
-      int to = curr[1];
-
-      if (visited[to]) continue;
-      visited[to] = true;
-      total += cost;
-      visitedCount++;
-
-      if (visitedCount == V) break;
-      
-      for (int[] next : graph.get(to)) {
-        int nextCost = next[1];
-        int nextTo  = next[0];
-
-        if (!visited[nextTo]) {
-          pq.add(new int[]{nextCost, nextTo});
+class Main  {
+    static int V, E;
+    static List<List<int[]>> graph;
+    static PriorityQueue<int[]> pq;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] st = br.readLine().split(" ");
+        
+        V = Integer.parseInt(st[0]);
+        E = Integer.parseInt(st[1]);
+        
+        graph = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+          graph.add(new ArrayList<>());
         }
+        visited = new boolean[V];
+        
+        // {cost, to} cost 오름차순
+        pq = new PriorityQueue<>(
+          (a, b) -> Integer.compare(a[0], b[0])
+        );
+        
+        for (int i = 0; i < E; i++) {
+          String[] row = br.readLine().split(" ");
+
+          int a = Integer.parseInt(row[0]) -1;
+          int b = Integer.parseInt(row[1]) -1;
+          int c = Integer.parseInt(row[2]);
+
+          graph.get(a).add(new int[]{b, c});
+          graph.get(b).add(new int[]{a, c});
+        }
+
+        pq.add(new int[]{0, 0});
+        
+        calc();
+    }
+    static void calc() {
+      int total = 0;
+      while (!pq.isEmpty()) {
+
+        int[] curr = pq.poll();
+
+        int cost = curr[0];
+        int to = curr[1];
+
+        if (visited[to]) continue;
+        visited[to] = true;
+        total += cost;
+
+        for (int[] next : graph.get(to)) {
+          int nextTo = next[0];
+          int nextCost = next[1];
+
+          if (!visited[nextTo]) {
+            pq.add(new int[]{nextCost, nextTo});
+          }
+        }
+        
       }
+
+      System.out.println(total);
     }
 
-    System.out.println(total);
 
 
-  }
-} 
+}
