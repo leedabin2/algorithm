784ad949360e11b2753dec0c736cd340b778ed56
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2026-01-04 11:18:01 +0900

    [Gold IV] Title: 스도쿠, Time: 408 ms, Memory: 17932 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,65 +1,77 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-  static int[][] board = new int[9][9];
-  static List<int[]> blanks = new ArrayList<>();
-  public static void main(String[] args) throws IOException {
-    BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+  static final int N = 9;
+  static int[][] board;
+  static boolean isFound = false;
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    for (int i = 0; i < 9; i++) {
-      String[] st = br.readLine().split(" ");
-      for (int j = 0 ; j < 9; j++) {
-        board[i][j] = Integer.parseInt(st[j]);
-        if (board[i][j] == 0) blanks.add(new int[]{i,j});
+      board = new int[N][N];
+      List<int[]> blanks = new ArrayList<>();
+      for (int i = 0; i < N; i++) {
+        String[] row = br.readLine().split(" ");
+        for (int j = 0; j < N; j++) {
+          board[i][j] = Integer.parseInt(row[j]);
+          if (board[i][j] == 0) blanks.add(new int[]{i, j});
+        }
       }
-    }
 
-    sudoku(0);
-  }
-  static void sudoku(int depth) {
+      dfs(0, blanks);
+   }
+   static void dfs(int depth, List<int[]> blanks) {
+    if (isFound) return;
+
     if (depth == blanks.size()) {
       printBoard();
-      System.exit(0);
+      isFound = true;
+      return;
     }
 
     int x = blanks.get(depth)[0];
     int y = blanks.get(depth)[1];
 
     for (int i = 1; i <= 9; i++) {
-      if (isValid(x, y, i)) {
+      if (check(i, x, y)) {
         board[x][y] = i;
-        sudoku(depth + 1);
+        dfs(depth + 1, blanks);
         board[x][y] = 0;
       }
     }
-    
-  }
+   }
 
-  static boolean isValid(int x, int y, int num) {
-    // 가로 세로
-    for (int i = 0; i < 9; i++) {
-      if (board[x][i] == num || board[i][y] == num) return false;
+   static boolean check(int num, int x, int y ) {
+    // 가로, 세로 체크
+    for (int i = 0; i < N; i++) {
+      if (board[x][i] == num) return false;
+      if (board[i][y] == num) return false;
     }
-    
+
     // 3 * 3
     int sx = (x / 3) * 3;
     int sy = (y / 3) * 3;
-    for (int i = sx; i < sx + 3; i++) {
-      for (int j = sy; j < sy + 3; j++) {
+    // 0, 1, 2 -> 0
+    // 3, 4, 5 -> 3
+    // 6, 7, 8 -> 6
+    for (int i = sx; i < sx+3; i++) {
+      for (int j = sy; j < sy+3; j++) {
         if (board[i][j] == num) return false;
       }
     }
 
-    return true;
-  }
 
-  static void printBoard() {
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0 ; j < 9; j++) {
-          System.out.print(board[i][j] + " ");
+    return true;
+   }
+
+   static void printBoard() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        sb.append(board[i][j]).append(" ");
       }
-      System.out.println();
+      sb.append("\n");
     }
-  }
-} 
+    System.out.println(sb);
+   }
+}
