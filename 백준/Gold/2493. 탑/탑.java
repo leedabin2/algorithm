151197c626c57180216bbc25968b190552437c05
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2026-01-05 13:02:52 +0900

    [Gold V] Title: 탑, Time: 620 ms, Memory: 95248 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,38 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    String[] st = br.readLine().split(" ");
-    int[] height = new int[N];
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    for (int i = 0; i < N; i++) {
-      height[i] = Integer.parseInt(st[i]);
-    }
+      int N = Integer.parseInt(br.readLine());
 
-    Stack<Integer> stack = new Stack<>();  
-    int[] answer = new int[N];
-
-    for (int i = 0 ; i < N; i++) {
-
-      while(!stack.isEmpty() && height[stack.peek()] < height[i] ) {
-        stack.pop();
+      int[] top = new int[N];
+      String[] st = br.readLine().split(" ");
+      for (int i = 0; i < N; i++) {
+        top[i] = Integer.parseInt(st[i]);
       }
 
-      if (!stack.isEmpty()) {
-        answer[i] = stack.peek() + 1;
-      } else {
-        answer[i] = 0;
+      Deque<Integer> stack = new ArrayDeque<>();
+      int[] answer = new int[N];
+
+      for (int i = N-1; i >= 0; i--) {
+        
+        while (!stack.isEmpty() && top[stack.peek()] <= top[i]) {
+          int pop = stack.poll();
+          answer[pop] = i+1;
+        }
+
+        stack.addFirst(i);
       }
 
-      stack.push(i);
-    }
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < N; i++) {
+        sb.append(answer[i]).append(" ");
+      }
 
-    for (int a : answer) {
-      System.out.print(a + " ");
-    }
+      System.out.println(sb.toString().trim());
+   }
 
-  }
 }
