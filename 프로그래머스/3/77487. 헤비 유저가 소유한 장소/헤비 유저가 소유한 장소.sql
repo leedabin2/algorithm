05d3d63f10166d9129d2af10110eb544ff411cdc
leedabin2 <122510655+leedabin2@users.noreply.github.com>
2026-01-05 22:45:54 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,9 +1,11 @@
 -- 코드를 입력하세요
-SELECT ID, NAME, HOST_ID
-from PLACES
-where host_id in (
-select host_id
-from PLACES
-group by host_id
-having count(*) >= 2)
-order by ID;
+SELECT p.ID, p.NAME, p.HOST_ID
+FROM PLACES p
+join (
+    select HOST_ID
+    from places 
+    group by HOST_ID
+    HAVING count(*) >= 2
+) h 
+on p.HOST_ID = h.HOST_ID
+order by p.ID;
